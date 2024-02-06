893c2f456f76b86b4166f96839838c4766e368c7
ArtemZen <azenkevich@kvant-is.ru>
2024-02-06 16:31:12 +0300

    v 1.0.0

--- ton-vue/src/App.vue
+++ ton-vue/src/App.vue
@@ -1,64 +1,204 @@
 <template>
   <div>
-    <DataTable :value="users" paginator :rows="pageSize" :totalRecords="totalUsers"
+
+    <DataTable :value="users" lazy :first="first" paginator :rows="pageSize" :totalRecords="totalUsers"
                paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-               :rowsPerPageOptions="[5,10,20]" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} users">
-      <Column field="first_name" header="First Name"></Column>
-      <Column field="last_name" header="Last Name"></Column>
-      <Column field="email" header="Email"></Column>
-      <Column field="country" header="Country"></Column>
+               :rowsPerPageOptions="[5,10,20, 50, 100]" @page="loadUsers($event)" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} users" class="mt-4">
+      <template #header>
+        <div class="flex justify-content-between">
+          <IconField iconPosition="left">
+            <InputIcon>
+              <i class="pi pi-search" />
+            </InputIcon>
+            <InputText v-model="search" @input="handleSearchChange" placeholder="Keyword Search" />
+          </IconField>
+        </div>
+      </template>
+      <template #empty> No customers found. </template>
+      <Column field="profile_picture"  header="Profile Picture" class="min-width-150">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100px" height="100px" />
+          <img v-else :src="slotProps.data.profile_picture" alt="Profile Picture" style="max-width: 100px;" />
+        </template>
+      </Column>
+      <Column header="First Name" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.first_name }}</span>
+        </template>
+      </Column>
+      <Column header="Last Name" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.last_name}}</span>
+        </template>
+      </Column>
+      <Column header="Email" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.email }}</span>
+        </template>
+      </Column>
+      <Column field="country" header="Country" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.country }}</span>
+        </template>
+      </Column>
+      <Column field="phone" header="Phone" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.phone }}</span>
+        </template>
+      </Column>
+      <Column field="street" header="Street" class="min-width-400">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.street }}</span>
+        </template>
+      </Column>
+      <Column field="state" header="State" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.state }}</span>
+        </template>
+      </Column>
+      <Column field="longitude" header="Longitude" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.longitude }}</span>
+        </template>
+      </Column>
+      <Column field="gender" header="Gender" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.gender }}</span>
+        </template>
+      </Column>
+      <Column field="date_of_birth" header="Date of Birth" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.date_of_birth }}</span>
+        </template>
+      </Column>
+      <Column field="job" header="Job" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.job }}</span>
+        </template>
+      </Column>
+      <Column field="city" header="City" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.city }}</span>
+        </template>
+      </Column>
+      <Column field="zipcode" header="Zipcode" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.zipcode }}</span>
+        </template>
+      </Column>
+      <Column field="latitude" header="Latitude" class="min-width-200">
+        <template #body="slotProps">
+          <Skeleton v-if="loading" width="100%" height="1rem" />
+          <span v-else>{{ slotProps.data.latitude }}</span>
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import  DataTable from 'primevue/datatable';
+import Skeleton from 'primevue/skeleton';
+import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
 export default {
   components: {
     DataTable,
     Column,
+    Skeleton,
+    IconField,
+    InputIcon,
+    InputText
   },
   data() {
     return {
+      loading:false,
+      first:0,
       users: [],
       currentPage: 1,
       pageSize: 10,
       totalUsers: 0,
+      search:"",
       apiUrl: 'https://api.slingacademy.com/v1/sample-data/users',
     };
   },
   methods: {
-    loadUsers() {
+    handleSearchChange() {
+      // Используем задержку выполнения запроса после завершения ввода
+      clearTimeout(this.searchTimeout);
+      this.searchTimeout = setTimeout(() => {
+        this.loadUsers();
+      }, 500); // Задержка в миллисекундах (0.5 секунды), можно настроить под свои потребности
+    },
+    loadUsers(event=null) {
+      this.loading = true
+      console.log(event)
+      if (event){
+        this.first = event.first;
+        this.pageSize = event.rows
+      }
+      else{
+        this.first = 0;
+        this.pageSize = 5;
+      }
+      console.log("1", this.first, this.pageSize, this.search)
       axios.get(this.apiUrl, {
         params: {
-          offset: (this.currentPage - 1) * this.pageSize,
+          offset: this.first,
           limit: this.pageSize,
+          search: this.search
         },
       })
           .then(response => {
-            // Обработка данных для соответствия ожидаемой структуре DataTable
             this.users = response.data.users.map(user => ({
               ...user,
-              name: `${user.first_name} ${user.last_name}`,
-              country: user.country,
             }));
+            console.log(response.data)
             this.totalUsers = response.data.total_users;
+            console.log("2",this.users)
           })
           .catch(error => {
             console.error('Ошибка при загрузке пользователей:', error);
+          }).finally(() => {
+            this.loading = false; // Установка после завершения запроса
           });
     },
   },
-  watch: {
-    // Просмотр изменений для currentPage и pageSize, чтобы перезагружать данные
-    currentPage: 'loadUsers',
-    pageSize: 'loadUsers',
+  created() {
+    this.loadUsers();
   },
   mounted() {
     this.loadUsers();
-  },
+  }
 };
 </script>
+<style scoped>
+:global(span.p-column-title) {
+  font-weight: bold;
+}
+.min-width-150 {
+  min-width: 150px;
+}
+.min-width-200 {
+  min-width: 200px;
+}
+.min-width-400 {
+  min-width: 400px;
+}
+</style>
--- ton-vue/src/main.js
+++ ton-vue/src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
-
-
+import 'primevue/resources/themes/md-dark-indigo/theme.css'; // Замените 'saga-blue' на выбранную вами тему
+import 'primevue/resources/primevue.min.css';
 const app = createApp(App);
 
-app.use(PrimeVue);
+app.use(PrimeVue, { theme: 'md-dark-indigo' });
 
 app.mount('#app');
